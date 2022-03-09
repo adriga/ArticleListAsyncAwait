f16b10579cf66b0740a5579dfbc80b6fb7ede092
Adrian Gil Alonso <adrian.gil.contractor@bbva.com>
2022-03-09 16:56:48 +0100

    Naming changes

--- ArticleListV2Tests/Core/DataSources/Network/NetworkManagerTests.swift
+++ ArticleListV2Tests/Core/DataSources/Network/NetworkManagerTests.swift
@@ -27,7 +27,7 @@ class NetworkManagerTests: XCTestCase {
         }
         
         // When
-        let response = await sut.makeRequest(request: TestGetApiService())
+        let response = await sut.makeRequest(request: GetApiServiceMock())
         
         // Then
         switch response {
@@ -46,7 +46,7 @@ class NetworkManagerTests: XCTestCase {
         }
         
         // When
-        let response = await sut.makeRequest(request: TestGetApiService())
+        let response = await sut.makeRequest(request: GetApiServiceMock())
         
         // Then
         switch response {
@@ -66,7 +66,7 @@ class NetworkManagerTests: XCTestCase {
         }
 
         // When
-        let response = await sut.makeRequest(request: TestGetApiService())
+        let response = await sut.makeRequest(request: GetApiServiceMock())
 
         // Then
         switch response {
